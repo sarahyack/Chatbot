6355fc1dade41e2a1a4f7d88788d724ee4a6f791
Sarah Yack <sarah@yack.email>
2024-01-23 14:11:12 -0700

    Setting Up File Organization
    Description:
    
    v0.0.1

--- file_setup/config.py
+++ file_setup/config.py
@@ -1,7 +1,7 @@
 grade_6_english = "C:\\Users\\Sarah\\Documents\\School\\Ron Paul Curriculum\\Grade 6\\Grade 6 English\\Essay posts"
 grade_6_history = "C:\\Users\\Sarah\\Documents\\School\\Ron Paul Curriculum\\Grade 6\\Grade 6 History\\Review posts"
 grade_7_english = "C:\\Users\\Sarah\\Documents\\School\\Ron Paul Curriculum\\Grade 7\\Grade 7 English\\Essays for 7 Grade Literature"
-grade_7_history = "C:\\Users\\Sarah\\Documents\\School\\Ron Paul Curriculum\\Grade 7\\Grade 7 History\\Essays for 7 Grade History"
+grade_7_history = "C:\\Users\\Sarah\\Documents\\School\\Ron Paul Curriculum\\Grade 7\\Grade 7 History\\Essays for 7th Grade History"
 # Multi-Line comment out the variables below
 # grade_8_essays =
 # grade_9_essays =
@@ -10,3 +10,5 @@ grade_7_history = "C:\\Users\\Sarah\\Documents\\School\\Ron Paul Curriculum\\Gra
 # grade_12_essays =
 
 paths = [grade_6_english, grade_6_history, grade_7_english, grade_7_history]
+
+target_dir = "C:\\Users\\Sarah\\AI-Creation\\Machine Learning\\Datasets\\Essays\\"
